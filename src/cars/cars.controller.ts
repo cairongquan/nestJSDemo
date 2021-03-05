8bee7c6746bc24fa6dd49240c26c038a238655bd
crq <crq@aaa.com>
2021-03-05 17:11:46 +0800

    add:动态路由

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,15 +1,39 @@
-import { Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post, Put, Query, Res } from '@nestjs/common';
 import { CarsService } from './cars.service';
 import { resType, carType } from 'Interfaces/myInterface';
+import { Response } from 'express'
 
+// http://localhost:8087/cars/xxxx 路由
 
 @Controller('cars')
 export class CarsController { //导出控制器类
     constructor(private readonly carsService: CarsService) { //构造器 引入service层代码
     }
 
+
+    // 动态路由
+    /*
+        例如 http://localhost:8087/cars/tran/1  params = {id:1}
+        例如 http://localhost:8087/cars/tran/2  params = {id:2}
+    */
+    @Get('/tran/:id') //动态路由
+    tranRouter(@Param('id') id: number) {
+        console.log(id);
+        return id;
+    }
+
+    
+    // {key:1}
+    // @Query('key') 将key属性的值提取出来 
     @Get('/getCarsList') //查看所有车辆列表
-    getAllCarsList(): resType {
+    getAllCarsList(@Query('name') name: string): resType {
+        if (!name) {
+            return {
+                msg: "数据出错",
+                data: null,
+                code: 505
+            }
+        }
         return this.carsService.getAllCars();
     }
 
